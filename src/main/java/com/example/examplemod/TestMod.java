dd81e45338ae8ae9ab7c73d038774a32992e5e4f
amarraja123 <amar.arawat@gmail.com>
2018-01-13 00:07:23 -0500

    Initial commit

---
+++ src/main/java/com/example/examplemod/TestMod.java
@@ -0,0 +1,40 @@
+package com.example.examplemod;
+
+import com.example.examplemod.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by amarendra on 12/01/18.
+ */
+@Mod(modid = Reference.MODID, version = Reference.VERSION, name = Reference.NAME,
+        acceptedMinecraftVersions = Reference.ACCEPTED_VERSION)
+public class TestMod {
+
+    @Instance
+    public static TestMod instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+
+}
